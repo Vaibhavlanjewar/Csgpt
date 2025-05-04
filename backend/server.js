71939f4059164028faa19062d94add7f43f1c7f3
Vaibhav Nivrutti Lanjewar <105924348+Vaibhavlanjewar@users.noreply.github.com>
2025-05-04 13:15:12 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,13 @@ const apiRouter = require("./routes/api");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+
+
+app.use(cors({
+  origin: 'https://csgpt-frontend.onrender.com',
+  methods: ['GET', 'POST'],
+  credentials: true
+}));
 app.use(express.json());
 app.use("/api", apiRouter);
 
